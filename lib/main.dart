ff7eff1c555554e287042080fd30e15f465ef2f3
Jingran Wang <hikaritenchi@gmail.com>
2023-12-06 15:18:04 -0500

    Add basic riverpod and FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jasper_weight_tracker/firebase_options.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+@riverpod
+Stream<User?> loggedInUser(LoggedInUserRef ref) async* {
+  FirebaseAuth.instance.authStateChanges();
+}
+
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loggedInUser = ref.watch(loggedInUserProvider);
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: switch (loggedInUser) {
+          AsyncValue(:final value) => null == value
+              ? MyHomePage(title: "logged out")
+              : MyHomePage(title: "logged in"),
+        });
   }
 }
 
---
+++ lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loggedInUserHash() => r'5060b33741a3ad56ac3bad03490ba5a0376922da';
+
+/// See also [loggedInUser].
+@ProviderFor(loggedInUser)
+final loggedInUserProvider = AutoDisposeStreamProvider<User?>.internal(
+  loggedInUser,
+  name: r'loggedInUserProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loggedInUserHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LoggedInUserRef = AutoDisposeStreamProviderRef<User?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
